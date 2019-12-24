19cd32d4e6aa25501684b1def2d6228abe4f547e
Ara <aghiulezian@gmail.com>
2019-12-24 12:53:08 +0400

    fixed bug in printBoard

--- MazeView.java
+++ MazeView.java
@@ -30,66 +30,73 @@ public class MazeView{
 
     MazeView(MazeGenerator maze){
         this.myMaze = maze;
-        this.viewboard = new int[(maze.dimensions * 2) - 1][(maze.dimensions * 2) - 1];
+        this.viewboard = new int[(maze.dimensions * 2) + 1][(maze.dimensions * 2) + 1];
     }
 
 
     public void printBoard(){
-        for (int y = 0; y < this.viewboard.length; y++){  // rows
-            
-            for (int x = 0 ; x < this.viewboard.length; x++){
-                //System.out.print(y == 0 || y == (this.viewboard.length - 1)? "*  ": "w  ");
-                //System.out.print( (y == 0 || y == (this.viewboard.length - 1) || (x == 0 || x == (this.viewboard.length - 1)) ? "*  ": "w  "));
+
+        for (int cellY = 0, y = 0; y < this.viewboard.length; y++){  // rows   
+            if(y % 2 != 0 && y != 1){
+                cellY += 1;
+            }        
+            for (int cellX = 0, x = 0 ; x < this.viewboard.length; x++){
+                if(x % 2 != 0 && x != 1){
+                    cellX += 1;
+                }    
+                Directions dir = this.myMaze.directions.get(new Point(cellX,cellY));
                 System.out.print( (y == 0 || y == (this.viewboard.length - 1) || (x == 0 || x == (this.viewboard.length - 1)) ? "*  ": 
-                (y%2 != 0 && x%2 != 0)?"   ":(y%2 == 0 && x%2 == 0)? "*  ":"w  " ));
+                (y%2 != 0 && x%2 != 0)?"   ":(y%2 == 0 && x%2 == 0)? "*  ":
+                (y%2 == 0 && dir.UP)?"   ":
+                (y%2 != 0 && dir.RIGHT)?"   ":"w  " ));
             }
             System.out.println();
         }
     }
 
-    // public void printBoard(){
-    //     int max = (myMaze.dimensions * 2) + 1;
-    //     int y = 0;
-    //     for (int i = 0 ; i < max ; i++){
-    //         if (i % 2 == 0){
-    //             int x = 0;
-    //             for (int j = 0 ; j < ((myMaze.dimensions * 2) + 1); j++){  // if j odd -->  maybe  UP/DOWN wall
-    //                 if(j%2 != 0 && j != 1){
-    //                     x += 1;
-    //                 }
+    public void printBoard2(){
+        int max = (myMaze.dimensions * 2) + 1;
+        int y = 0;
+        for (int i = 0 ; i < max ; i++){
+            if (i % 2 == 0){
+                int x = 0;
+                for (int j = 0 ; j < ((myMaze.dimensions * 2) + 1); j++){  // if j odd -->  maybe  UP/DOWN wall
+                    if(j%2 != 0 && j != 1){
+                        x += 1;
+                    }
 
 
-    //                 //  we've got to check if the down wall for point(x,y) exists
-    //                 Point searchPoint = new Point(x,y);
-    //                 if (i != max - 1 && myMaze.directions.get(searchPoint).DOWN == true){
-    //                     System.out.print(j == (myMaze.dimensions * 2)? "*": (j % 2 != 0 && i != 0 && i != myMaze.dimensions*2)? "   ": "*  ");
+                    //  we've got to check if the down wall for point(x,y) exists
+                    Point searchPoint = new Point(x,y);
+                    if (i != max - 1 && myMaze.directions.get(searchPoint).DOWN == true){
+                        System.out.print(j == (myMaze.dimensions * 2)? "*": (j % 2 != 0 && i != 0 && i != myMaze.dimensions*2)? "   ": "*  ");
 
-    //                 }
-    //                 else
-    //                 {
-    //                     System.out.print(j == (myMaze.dimensions * 2)? "*": (j % 2 != 0 && i != 0 && i != myMaze.dimensions*2)? "*  ": "*  ");
-    //                 }
+                    }
+                    else
+                    {
+                        System.out.print(j == (myMaze.dimensions * 2)? "*": (j % 2 != 0 && i != 0 && i != myMaze.dimensions*2)? "*  ": "*  ");
+                    }
 
                     
-    //             }
-    //             System.out.println();        
-    //         }
-    //         else{
-    //             int x = 0;
-    //             for (int j = 0 ; j < ((myMaze.dimensions*2) + 1); j++){      // odd j's are cells! ---> even j's are LEFT/RIGHT
-    //                 if (j % 2 != 0 && j != 1){
-    //                     x += 1;
-    //                 }
-    //                 Point searchPoint = new Point(x,y);
-    //                 System.out.print(j%2 != 0? "     ": j == 0 || j == myMaze.dimensions*2 ? "*": 
-    //                 myMaze.directions.get(searchPoint).RIGHT == true? " ":"*");
+                }
+                System.out.println();        
+            }
+            else{
+                int x = 0;
+                for (int j = 0 ; j < ((myMaze.dimensions*2) + 1); j++){      // odd j's are cells! ---> even j's are LEFT/RIGHT
+                    if (j % 2 != 0 && j != 1){
+                        x += 1;
+                    }
+                    Point searchPoint = new Point(x,y);
+                    System.out.print(j%2 != 0? "     ": j == 0 || j == myMaze.dimensions*2 ? "*": 
+                    myMaze.directions.get(searchPoint).RIGHT == true? " ":"*");
 
-    //             }
-    //             y += 1;                
-    //             System.out.println();
-    //         }
-    //     }
-    // }
+                }
+                y += 1;                
+                System.out.println();
+            }
+        }
+    }
 
 
     public static void main(String[] args) 
@@ -98,6 +105,9 @@ public class MazeView{
 
         MazeView view = new MazeView(myMaze);
         view.printBoard();
+        System.out.println(view.viewboard.length);
+        System.out.println();
+        view.printBoard2();
         //MazeSolver.printBoard();
     
 
