e33c62049a6cf81b137d9d39d1e58d9f8222d62f
Ara <aghiulezian@gmail.com>
2020-03-23 14:20:56 -0700

    complete rework from this point

--- Directions.java
+++
@@ -1,6 +0,0 @@
-public class Directions{
-    public boolean UP = false;
-    public boolean RIGHT = false;
-    public boolean DOWN  = false;
-    public boolean LEFT  = false;
-}
--- Globals.java
+++
@@ -1,4 +0,0 @@
-public class Globals{
-    public static MazeGenerator myMaze = new MazeGenerator(15);
-    public static MazeView myView = new MazeView(myMaze);
-}
